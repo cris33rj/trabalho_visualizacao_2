c8d7800e7727860d360662e7bccf1c0d8502db91
= <=>
2024-05-06 19:07:49 -0300

    ajustes adicionais e inclusão de mais gráficos

--- docs/comparacao_contagem_total_plataformas.csv.py
+++ docs/comparacao_contagem_total_plataformas.csv.py
@@ -1,14 +1,12 @@
 # %%
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 import sys
 
 # %%
-# Attempting to load the dataset using 'ISO-8859-1' encoding
+# 
 spotify_data = pd.read_csv('docs/data/spotify-2023.csv', encoding='ISO-8859-1')
 
-# Displaying the first few rows of the dataset
-#spotify_data.head()
 
 
 # %%
@@ -23,18 +21,13 @@ teste = spotify_data['in_deezer_playlists'].sum()
 #print(teste)
 
 # %%
-#spotify_data['in_deezer_charts'] = spotify_data['in_deezer_charts'].apply(remover_virgula)
-#teste = spotify_data['in_deezer_charts'].sum()
-#print(teste)
+
 
 # %%
 spotify_data['in_shazam_charts'] = spotify_data['in_shazam_charts'].apply(remover_virgula)
 
-#teste3 = spotify_data['in_shazam_charts'].sum()
-#print(teste3)
 
 # %%
-# Calculating the total count of tracks present in playlists/charts for Spotify and Apple Music
 total_counts = {
     'Spotify': spotify_data['in_spotify_playlists'].sum() + spotify_data['in_spotify_charts'].sum(),
     'Apple Music': spotify_data['in_apple_playlists'].sum() + spotify_data['in_apple_charts'].sum(),
@@ -46,22 +39,18 @@ total_counts = {
 
 
 # %%
-#print(total_counts)
+
 
 # %%
 columns = ['plataforma', 'contagem']
 
-# Create a DataFrame
-#df_total_counts = pd.DataFrame(total_counts, index=total_counts.keys(), data=total_counts.values(), columns=columns)
 df_total_counts  = pd.DataFrame(list(total_counts.items()), columns=['plataforma', 'contagem'])
 
 # %%
 df_total_counts.head()
 
 # %%
-#df_total_counts_spotify_apple_deezer_shazam.head()
 
 # %%
 df_total_counts.to_csv(sys.stdout,header=True,index=False)
-
 
--- docs/top_10_songs_apple_playlists_charts.csv.py
+++ docs/top_10_songs_apple_playlists_charts.csv.py
@@ -1,6 +1,6 @@
 # %%
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from itertools import combinations
@@ -8,7 +8,7 @@ import sys
 
 import warnings
 
-warnings.filterwarnings("ignore")  # Ignores all warnings
+warnings.filterwarnings("ignore")  
 
 
 spotify_data = pd.read_csv('docs/data/spotify-2023.csv', encoding='ISO-8859-1')
@@ -29,22 +29,22 @@ spotify_data['in_shazam_charts'] = spotify_data['in_shazam_charts'].apply(remove
 
 
 # %%
-# Identifying top 10 songs for Spotify based on their presence in playlists and charts
+# Descobrindo as 10 músicas mais tocadas do Spotify com base em presença em playlists e paradas.
 top_songs_spotify = spotify_data[['track_name', 'artist(s)_name', 'in_spotify_playlists', 'in_spotify_charts']]
 top_songs_spotify['spotify_total'] = top_songs_spotify['in_spotify_playlists'] + top_songs_spotify['in_spotify_charts']
 top_songs_spotify = top_songs_spotify.sort_values(by='spotify_total', ascending=False).head(10)
 
-# Identifying top 10 songs for Apple Music based on their presence in playlists and charts
+# Descobrindo as 10 músicas mais tocadas da Apple com base em presença em playlists e paradas.
 top_songs_apple = spotify_data[['track_name', 'artist(s)_name', 'in_apple_playlists', 'in_apple_charts']]
 top_songs_apple['apple_total'] = top_songs_apple['in_apple_playlists'] + top_songs_apple['in_apple_charts']
 top_songs_apple = top_songs_apple.sort_values(by='apple_total', ascending=False).head(10)
 
-# Identifying top 10 songs for Apple Music based on their presence in playlists and charts
+# Descobrindo as 10 músicas mais tocadas do Deezer com base em presença em playlists e paradas.
 top_songs_deezer = spotify_data[['track_name', 'artist(s)_name', 'in_deezer_playlists', 'in_deezer_charts']]
 top_songs_deezer['deezer_total'] = top_songs_deezer['in_deezer_playlists'] + top_songs_deezer['in_deezer_charts']
 top_songs_deezer = top_songs_deezer.sort_values(by='deezer_total', ascending=False).head(10)
 
-# Identifying top 10 songs for Apple Music based on their presence in playlists and charts
+# Descobrindo as 10 músicas mais tocadas do Shazam com base em presença em playlists e paradas.
 top_songs_shazam = spotify_data[['track_name', 'artist(s)_name', 'in_shazam_charts']]
 top_songs_shazam['shazam_total'] = top_songs_shazam['in_shazam_charts']
 top_songs_shazam = top_songs_shazam.sort_values(by='shazam_total', ascending=False).head(10)
